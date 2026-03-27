a0e393af851e0893ad98ec82a611bdf3f263e825
рыжик <@DESKTOP-RQ938JG>
2026-03-27 13:41:09 +0800

    SolarRent исправление ошибок

--- SolarRent/SolarRent.Core/Model/User.cs
+++ SolarRent/SolarRent.Core/Model/User.cs
@@ -13,11 +13,8 @@ namespace SolarRent.Core.Models
     public class User
     {
         public int Id { get; set; }
-        [Required, MaxLength(100)]
         public string FullName { get; set; } = string.Empty;
-        [Required, MaxLength(50)]
         public string Login { get; set; } = string.Empty;
-        [Required]
         public string PasswordHash { get; set; } = string.Empty;
         public Role Role { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
